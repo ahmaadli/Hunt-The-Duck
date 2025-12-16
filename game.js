2d186a25cc83f2cb412ff43a4524e80d6cab69eb
Ahmad <163461779+ahmaadli@users.noreply.github.com>
2025-12-16 22:51:01 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var config = {
+        width: 800,
+        height: 600,
+        backgroundColor: '#87CEEB',
+    }
+    var HuntTheDuck = new Phaser.Game(config);
+}
