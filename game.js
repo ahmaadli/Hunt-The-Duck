1c66fad90b60ff66253098db1fb0f9b890700a55
Ahmad <163461779+ahmaadli@users.noreply.github.com>
2025-12-17 22:12:34 +0300

    Ahmad

--- game.js
+++ game.js
@@ -1,34 +1,8 @@
-create() {
-this.courseKeys = this.InputDeviceInfo.keyboard.createCursorKeys();//للكيبورد
-
-
-var KeyW = scene.input.Keyboard.addKey("w");
-var KeyW = scene.input.Keyboard.addKey("D");
-var KeyW = scene.input.Keyboard.addKey("A");
-var KeyW = scene.input.Keyboard.addKey("S");
-
-}
-
-
-
-
-
-
-
-update() {
-var pointer =this.InputDeviceInfo.activePointer;
-if(pointer.isDown){ //للماوس
-var touchX = pointer.x;
-var touchY = pointer.y;
-}
-this.courseKeys = this.InputDeviceInfo.keyboard.createCursorKeys();
-var isUPDown = cursorKeys.up.isDown;//ازرار الاسهم
-var isDownDown = cursorKeys.down.isDown;
-var isLeftDown = cursorKeys.left.isDown;
-var isRightDown = cursorKeys.right.isDown;
-var isSpaceDown = cursorKeys.space.isDown;
-var isShiftDown = cursorKeys.shift.isDown;
-var isDown = KeyW.isDown;
-var isUp = KeyW.isUp;
-
+window.onload = function() {
+    var config = {
+        width: 800,
+        height: 600,
+        backgroundColor: '#87CEEB',
+    }
+    var HuntTheDuck = new Phaser.Game(config);
 }
