941fc1e48eedc6336e5698dbd451bfc14215bdbc
Ahmad <163461779+ahmaadli@users.noreply.github.com>
2025-12-18 18:41:36 +0300

    Update game configuration and input handling

--- game.js
+++ game.js
@@ -1,46 +1,19 @@
 window.onload = function() {
     var config = {
-        width: 900,
-        height: 600,
+        width: 1500,
+        height: 700,
         type: Phaser.AUTO,
         transparent: true,
         backgroundColor: '#ff0000ff',
-        scene: [FirstScene]
+        scene: [FirstScene],
+
+        physics: {
+            default: 'arcade',
+            arcade: {
+                debug: false
+            }
+        }
+
     }
     var HuntTheDuck = new Phaser.Game(config);
-}
-
-create(); {
-this.courseKeys = this.InputDeviceInfo.keyboard.createCursorKeys();//للكيبورد
-
-
-var KeyW = scene.input.Keyboard.addKey("w");
-var KeyW = scene.input.Keyboard.addKey("D");
-var KeyW = scene.input.Keyboard.addKey("A");
-var KeyW = scene.input.Keyboard.addKey("S");
-
-}
-
-
-//osammma
-
-
-
-
-update(); {
-var pointer =this.InputDeviceInfo.activePointer;
-if(pointer.isDown){ //للماوس
-var touchX = pointer.x;
-var touchY = pointer.y;
-}
-this.courseKeys = this.InputDeviceInfo.keyboard.createCursorKeys();
-var isUPDown = cursorKeys.up.isDown;//ازرار الاسهم
-var isDownDown = cursorKeys.down.isDown;
-var isLeftDown = cursorKeys.left.isDown;
-var isRightDown = cursorKeys.right.isDown;
-var isSpaceDown = cursorKeys.space.isDown;
-var isShiftDown = cursorKeys.shift.isDown;
-var isDown = KeyW.isDown;
-var isUp = KeyW.isUp;
-
 }
