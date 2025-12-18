9697def84a0b2c006e8d503d7b1df10f49f151b9
MS111 <mshm1424@gmail.com>
2025-12-18 13:57:43 +0300

    Update game.js

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ window.onload = function() {
         width: 800,
         height: 600,
         backgroundColor: '#87CEEB',
+        scene: [FirstScene]
     }
     var HuntTheDuck = new Phaser.Game(config);
 }
